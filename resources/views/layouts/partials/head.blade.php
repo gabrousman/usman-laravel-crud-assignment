381ee53ece156970b436ae144426ac780e3a8e51
Usman Tariq <gabrousman@gmail.com>
2018-01-18 13:44:15 +0500

    Add files via upload

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta charset="UTF-8" >
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" >
+        <meta content="width=device-width,initial-scale=1" name=viewport >
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <!-- mobile responsive specific Meta's -->
-        <meta content="width=device-width,initial-scale=1" name=viewport />
+
         <!-- SEO specific Meta's -->
-        <meta name="copyright" content="" />
-        <meta name="description" content="Test Job" />
-        <meta name="keywords" content="" />
-        <meta name="robots" content="index,follow" />
+        <meta name="copyright" content="" >
+        <meta name="description" content="Test Job" >
+        <meta name="keywords" content="" >
+        <meta name="robots" content="index,follow" >
         <title>Test Job</title>
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" >
         <!-- Custom css -->
-        <link href="{{ asset('css/custom.css') }}" rel="stylesheet" />
+        <link href="{{ asset('css/custom.css') }}" rel="stylesheet" >
         <!-- scripts -->
         <script src="{{ asset('js/jquery.min.js') }}"></script>
         <script src="{{ asset('js/app.js') }}"></script>
@@ -20,5 +23,5 @@
         <script type="text/javascript"> var APP_URL = {!! json_encode(url('/')) !!}</script>
     </head>
 
-    <body>
+    <body> 
         {{ csrf_field() }}
