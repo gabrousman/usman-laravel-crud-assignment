2297390025da0f8061bb8230c0d35de1374f0b92
Usman Tariq <gabrousman@gmail.com>
2018-01-18 21:41:16 +0500

    Add files via upload

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -58,7 +58,7 @@ class PagesController extends Controller {
         
         //Validate Extra Salery Values
         Validator::extend('PriceExtravalidate', function($attribute, $value, $parameters) {
-            $pattern = "/^[+-]?[0-9]{1,3}\s(?:[0-9]*(?:[.,][0-9]{2})?|(?:,[0-9]{3})*(?:\.[0-9]{2})?|(?:\.[0-9]{3})*(?:,[0-9]{2})?)$/";
+            $pattern = "/^[+-]?[0-9]{1,3}\s?(?:[0-9]*(?:[.,][0-9]{2})?|(?:,[0-9]{3})*(?:\.[0-9]{2})?|(?:\.[0-9]{3})*(?:,[0-9]{2})?)$/";
             $match = preg_match($pattern, $value);
             return $match;
         });
@@ -75,7 +75,6 @@ class PagesController extends Controller {
         if ($province == 'Québec') {
             $rules['salary'] = 'required|PriceExtravalidate';
         } else {
-
             $rules['salary'] = 'required|Pricevalidate';
         }
 
@@ -91,7 +90,15 @@ class PagesController extends Controller {
         $this->validate($request, $rules, $messages);
 
         //Save User
-        $user = new User();
+
+        $update_user = $request->input('user_id');
+        
+        if(empty($update_user)){
+            $user = new User();
+        }else{
+            $user = User::find($update_user);
+        }
+
         $user->name = $request->input('name');
         $user->province = $request->input('province');
         $user->phone = $request->input('phone');
@@ -131,72 +138,6 @@ class PagesController extends Controller {
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request) {
-        $province = $request->input('province');
-        
-        //Validate Phone Number
-        Validator::extend('CanadaPhone', function($attribute, $value, $parameters) {
-            $pattern = "/^(?:\(\d{3}\)|\d{3}-)\s?\d{3}-\d{4}$/";
-            $match = preg_match($pattern, $value);
-            return $match;
-        });
-        
-        //Validate Postal Codes
-        Validator::extend('CanadaPostCode', function($attribute, $value, $parameters) {
-            $pattern = "/^[ABCEGHJKLMNPRSTVXY]{1}\d{1}[A-Z]{1} *\d{1}[A-Z]{1}\d{1}$/";
-            $match = preg_match($pattern, $value);
-            return $match;
-        });
-        
-        //Validate Salery Values
-        Validator::extend('Pricevalidate', function($attribute, $value, $parameters) {
-            $pattern = "/^[+-]?[0-9]{1,3}(?:[0-9]*(?:[.,][0-9]{2})?|(?:,[0-9]{3})*(?:\.[0-9]{2})?|(?:\.[0-9]{3})*(?:,[0-9]{2})?)$/";
-            $match = preg_match($pattern, $value);
-            return $match;
-        });
-        
-        //Validate Extra Salery Values
-        Validator::extend('PriceExtravalidate', function($attribute, $value, $parameters) {
-            $pattern = "/^[+-]?[0-9]{1,3}\s(?:[0-9]*(?:[.,][0-9]{2})?|(?:,[0-9]{3})*(?:\.[0-9]{2})?|(?:\.[0-9]{3})*(?:,[0-9]{2})?)$/";
-            $match = preg_match($pattern, $value);
-            return $match;
-        });
-        
-        
-        $rules = array(
-            'name' => 'required|min:2',
-            'province' => 'required',
-            'phone' => 'required|CanadaPhone',
-            'postal_code' => 'required|CanadaPostCode'
-        );
-        
-        //For Salery Conditional Validation
-        if ($province == 'Québec') {
-            $rules['salary'] = 'required|PriceExtravalidate';
-        } else {
-
-            $rules['salary'] = 'required|Pricevalidate';
-        }
-
-        $messages = array(
-            '*.required' => 'This is required field.',
-            'name.min' => 'Name must be longer then 2 charecter.',
-            'phone.canada_phone' => 'allowed formats = (416) 123-4567 OR (416)123-4567 OR 416-123-4567',
-            'postal_code.canada_post_code' => 'Please enter a valid canada post code.',
-            'salary.pricevalidate' => 'Please enter a valid amount format.',
-            'salary.price_extravalidate' => 'Please enter a valid amount format.'
-        );
-        //Call Request Validator
-        $this->validate($request, $rules, $messages);
-        
-        $u_id = $request->input('user_id');
-        $user = User::find($u_id);
-        $user->name = $request->input('name');
-        $user->province = $request->input('province');
-        $user->phone = $request->input('phone');
-        $user->postal_code = $request->input('postal_code');
-        $user->salary = $request->input('salary');
-        $user->save();
-        return view('info_success', ['data' => $user, 'message' => 'Your data is saved.Go to listing page to see.']);
     }
 
     /**
